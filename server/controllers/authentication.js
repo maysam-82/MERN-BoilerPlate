01d52a6a3a21322238f49bb945332524003cdfa1
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-27 00:52:48 +1000

    create auth controller

---
+++ server/controllers/authentication.js
@@ -0,0 +1,35 @@
+const User = require('../models/user');
+exports.signup = function (req, res, next) {
+	//  pull data from request by the use of body. body means anything contains with post request.
+	const email = req.body.email;
+	const password = req.body.password;
+
+	// See if a user with a given email exits
+	User.findOne({ email: email }, function (error, existingUser) {
+		if (error) {
+			return next(error);
+		}
+
+		// if a user with email does exist, return an error
+		if (existingUser) {
+			return res.status(422).send({ error: 'Email has been already used' });
+		}
+
+		// if user does not exist, create and save user record.
+		// create a user in memory
+		const user = new User({
+			email: email,
+			password: password,
+		});
+
+		// save user in db
+		user.save(function (error) {
+			if (error) {
+				return next(error);
+			}
+
+			//  respond to request that user was created.
+			return res.json({ user });
+		});
+	});
+};
--- server/router.js
+++ server/router.js
@@ -1,9 +1,12 @@
+const Authentication = require('./controllers/authentication');
 module.exports = function (app) {
 	// if user visits the app with `/` route, then run callback function.
 	// req: (request) is an object that represent that incomming http request.
 	// res: (response) is an object that represent the respons which we are going to send back to that request.
 	// next: used for error handling.
-	app.get('/', function (req, res, next) {
-		res.send([1, 2, 3, 4, 5]);
-	});
+	// app.get('/', function (req, res, next) {
+	// 	res.send([1, 2, 3, 4, 5]);
+	// });
+
+	app.post('/signup', Authentication.signup);
 };
