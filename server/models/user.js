23a5415760fd790b935fc601521eed3995ac4ecf
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-27 01:14:55 +1000

    encrypt and save password with Bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt-nodejs');
 // Tell mongoose about very particular fileds that models going to have
 const Schema = mongoose.Schema;
 
@@ -7,6 +7,31 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
 	email: { type: String, unique: true, lowercase: true },
 	password: String,
+});
+
+// On save hook, encrypt password
+// Before saving a model, run this function
+userSchema.pre('save', function (next) {
+	// access to the context which is UserModel. user is an instance of a UserModel
+	const user = this;
+
+	// generate salt. It take some time and then run callback.
+	bcrypt.genSalt(10, function (error, salt) {
+		if (error) {
+			return next(error);
+		}
+
+		// hash (encrypt) password using the salt. it takes some time to run and then run a callback.
+		bcrypt.hash(user.password, salt, null, function (error, hash) {
+			if (error) {
+				return next(error);
+			}
+			// overwrite plain text password with an encrypted one.
+			user.password = hash;
+			// save a model.
+			next();
+		});
+	});
 });
 
 // Create the model class (Class of users)
