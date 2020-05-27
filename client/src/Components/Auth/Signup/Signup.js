f711db011bacd6618c9e9c5d6c57bb09ea145165
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-27 20:28:22 +1000

    implement redux-form to Signup

--- client/src/Components/Auth/Signup/Signup.js
+++ client/src/Components/Auth/Signup/Signup.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { reduxForm, Field } from 'redux-form';
 
-export default () => {
+const Signup = () => {
 	return (
 		<div>
 			<form>
 				<fieldset>
 					<label>Email</label>
+					<Field
+						name="email"
+						type="text"
+						component="input"
+						autoComplete="none"
+					/>
 				</fieldset>
 				<fieldset>
 					<label>Password</label>
+					<Field
+						name="password"
+						type="password"
+						component="input"
+						autoComplete="none"
+					/>
 				</fieldset>
 			</form>
 		</div>
 	);
 };
+
+export default reduxForm({ form: 'signup' })(Signup);
