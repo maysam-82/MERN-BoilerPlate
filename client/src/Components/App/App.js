179b380cb0ad8fca3609686538fcd7f730d83e3b
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-27 19:31:53 +1000

    Create header component

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 export default () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<Header />
+		</div>
+	);
 };
---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+export default () => {
+	return (
+		<div>
+			<Link to="/">Redux Auth</Link>
+			<Link to="/signup">Sign Up</Link>
+			<Link to="/signin">Sign In</Link>
+			<Link to="/signout">Sign Out</Link>
+			<Link to="/feature">Feature</Link>
+		</div>
+	);
+};
