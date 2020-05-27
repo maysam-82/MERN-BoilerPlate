042cfa4427148a672b4bbcbaf35d26e7fa876405
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-27 20:10:43 +1000

    create Signup and relevant route

---
+++ client/src/Components/Auth/Signup/Signup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			<form>
+				<fieldset>
+					<label>Email</label>
+				</fieldset>
+				<fieldset>
+					<label>Password</label>
+				</fieldset>
+			</form>
+		</div>
+	);
+};
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from './Components/App';
-import Welcome from './Components/Welcome/Welcome';
+import Welcome from './Components/Welcome';
+import Signup from './Components/Auth/Signup';
 
 ReactDOM.render(
 	<BrowserRouter>
 		<App>
 			<Route exact path="/" component={Welcome} />
+			<Route exact path="/signup" component={Signup} />
 		</App>
 	</BrowserRouter>,
 	document.querySelector('#root')
