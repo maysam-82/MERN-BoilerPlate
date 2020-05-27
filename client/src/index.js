9238b03a92c1ced3d8aed144626efbca0f62248d
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-27 20:37:36 +1000

    implelement redux-thunk middleware

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './Components/App';
 import Welcome from './Components/Welcome';
 import Signup from './Containers/Auth/Signup';
 import reducers from './reducers';
 
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
 ReactDOM.render(
-	<Provider store={createStore(reducers, {})}>
+	<Provider store={store}>
 		<BrowserRouter>
 			<App>
 				<Route exact path="/" component={Welcome} />
