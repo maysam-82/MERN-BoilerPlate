144ee93dd25472f2d57a046c764cbbe7ac475f3e
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-27 19:37:08 +1000

    wire up react-router and add Welcome to

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './Components/App';
+import Welcome from './Components/Welcome/Welcome';
 
 ReactDOM.render(
 	<BrowserRouter>
-		<App />
+		<App>
+			<Route exact path="/" component={Welcome} />
+		</App>
 	</BrowserRouter>,
 	document.querySelector('#root')
 );
