2c1975d17a9525c4b76c3e78a39698e48d788b2e
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-27 20:19:11 +1000

    create authReducer and connect redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './Components/App';
 import Welcome from './Components/Welcome';
 import Signup from './Components/Auth/Signup';
+import reducers from './reducers';
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App>
-			<Route exact path="/" component={Welcome} />
-			<Route exact path="/signup" component={Signup} />
-		</App>
-	</BrowserRouter>,
+	<Provider store={createStore(reducers, {})}>
+		<BrowserRouter>
+			<App>
+				<Route exact path="/" component={Welcome} />
+				<Route exact path="/signup" component={Signup} />
+			</App>
+		</BrowserRouter>
+	</Provider>,
 	document.querySelector('#root')
 );
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,10 @@
+INITIAL_STATE = {
+	authenticated: '',
+	errorMessage: '',
+};
+export default authReducer = (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
